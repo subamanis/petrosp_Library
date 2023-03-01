d0ca17ec85f211b9e56e1406bd14928b91424bca
Petros_Papa <petrospapa21@gmail.com>
2023-03-02 00:43:55 +0200

    Added Pair structure

---
+++ src/com/petrosp/structures/Pair.java
@@ -0,0 +1,31 @@
+package com.petrosp.structures;
+
+public class Pair<T, U> {
+    public T first;
+    public U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <T, U> Pair<T, U> of(T first, U second) {
+        return new Pair<>(first, second);
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    public boolean hasFirst() {
+        return first != null;
+    }
+
+    public boolean hasSecond() {
+        return second != null;
+    }
+}
